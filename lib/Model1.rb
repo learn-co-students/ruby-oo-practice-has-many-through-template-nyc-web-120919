8e19171755e58b17504f7ad1ddd4d4a81039c1c9
github username <github email address>
2019-12-15 14:12:53 -0500

    Done.

--- lib/Model1.rb
+++ lib/Model1.rb
@@ -1,5 +1,28 @@
-#class for Model1 goes here
-#Feel free to change the name of the class
-class Model1
+class Magazine
+    attr_reader :title, :monthly_cost
 
+    def initialize (title, monthly_cost)
+        @title = title
+        @monthly_cost = monthly_cost
+    end
+
+    def add_subscriber(person)
+        Subscription.new(person, self)
+    end
+
+    def unsubscribe(person)
+        Subscription.unsubscribe(person, self)
+    end
+
+    def subscribers
+        subscriptions.map {|subscription| subscription.person}
+    end
+
+    def subscriptions
+        Subscription.all.select {|subscription| subscription.magazine == self}
+    end
+
+    def monthly_revenue
+        subscriptions.size * @monthly_cost
+    end
 end
--- lib/Model2.rb
+++ lib/Model2.rb
@@ -1,5 +1,29 @@
-#class for Model2 goes here
-#Feel free to change the name of the class
-class Model2
+class Person
+
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+    end
+
+    def subscribe_to(magazine)
+        Subscription.new(self, magazine)
+    end
+
+    def unsubscribe(magazine)
+        Subscription.unsubscribe(self, magazine)
+    end
+
+    def magazines
+        subscriptions.map {|subscription| subscription.magazine}
+    end
+
+    def subscriptions
+        Subscription.all.select {|subscription| subscription.person == self}
+    end
+
+    def monthly_expenses
+        subscriptions.reduce(0.0) {|memo, subscription| memo += subscription.monthly_cost}
+    end
 
 end
